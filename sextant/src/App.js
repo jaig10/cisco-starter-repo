b158c6edccf63d8a87f3507b8ab54e62fbf33a4a
Jai <jaigaur05@gmail.com>
2023-05-15 02:46:53 +0530

    second task commit

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Box } from '@mui/material';
 import './App.css';
+import Cards from './Components/Cards';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Box>
+        <h1>Heading</h1>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+      </Box>
+    </>
   );
 }
 
